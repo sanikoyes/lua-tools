3b970a3d40765d847aa8d04da8145196b16e6b57
sanikoyes <sanikoyes@163.com>
2015-10-26 14:59:41 +0800

    add misc xxtea

---
+++ src/luamisc/luamisc.cpp
@@ -0,0 +1,51 @@
+#include "lua.hpp"
+
+#include <stdlib.h>
+#include "xxtea/xxtea.h"
+
+int l_xxtea_encrypt(lua_State *L) {
+
+	size_t data_len;
+	const char *data = luaL_checklstring(L, 1, &data_len);
+	size_t key_len;
+	const char *key = luaL_checklstring(L, 2, &key_len);
+
+	xxtea_long ret_length;
+	unsigned char *ret = xxtea_encrypt((unsigned char *) data, data_len, (unsigned char *) key, key_len, &ret_length);
+
+	lua_pushlstring(L, (char *) ret, ret_length);
+	free(ret);
+
+	return 1;
+}
+
+int l_xxtea_decrypt(lua_State *L) {
+
+	size_t data_len;
+	const char *data = luaL_checklstring(L, 1, &data_len);
+	size_t key_len;
+	const char *key = luaL_checklstring(L, 2, &key_len);
+
+	xxtea_long ret_length;
+	unsigned char *ret = xxtea_decrypt((unsigned char *) data, data_len, (unsigned char *) key, key_len, &ret_length);
+
+	lua_pushlstring(L, (char *) ret, ret_length);
+	free(ret);
+
+	return 1;
+}
+
+static const struct luaL_Reg xxtea_lib[] = {
+	{ "encrypt", l_xxtea_encrypt },
+	{ "decrypt", l_xxtea_decrypt },
+	{ NULL, NULL },
+};
+
+extern "C" {
+
+int luaopen_misc_xxtea(lua_State *L) {
+	luaL_newlib(L, xxtea_lib);
+	return 1;
+}
+
+};
